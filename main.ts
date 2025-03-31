5be776d16b490b17bb7d68725a2298f33f6e2c58
m-asaoka-fap <66696898+m-asaoka-fap@users.noreply.github.com>
2025-03-31 20:54:32 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    cuebit.back_10()
-})
-input.onButtonPressed(Button.AB, function () {
-    cuebit.brake()
-    cuebit.rightRotate_90()
-})
-input.onButtonPressed(Button.B, function () {
-    cuebit.forward_10()
-})
 cuebit.start()
 basic.forever(function () {
     cuebit.setorder(1000)
